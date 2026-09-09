79fd2bc0bb83937bab2ac76cc5033e5ed0b2a408
Johnny-Emad <johnnyjohnnyemnz@gmail.com>
2026-09-09 23:57:08 +0300

    Add skills relationship method to Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,6 +10,11 @@ class Project extends Model
     /** @use HasFactory<\Database\Factories\ProjectFactory> */
     use HasFactory;
 
+    public function skills()
+    {
+        return $this->belongsToMany(Skill::class);
+    }
+
     public function tasks()
     {
         return $this->hasMany(Task::class);
