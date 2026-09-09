2afe525c8b2f72069e700a5bcbe50add1ab583b3
Johnny-Emad <johnnyjohnnyemnz@gmail.com>
2026-09-09 18:54:28 +0300

    Refactor migration for profiles table to include schema creation and drop methods

--- database/migrations/2026_09_09_144100_creat_employee_training_table.php
+++ database/migrations/2026_09_09_144100_creat_employee_training_table.php
@@ -6,19 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained()->onDelete('cascade');
+            $table->foreignId('training_id')->constrained()->onDelete('cascade');
+            $table->date("completion_date")->nullable();
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('profiles');
     }
 };
