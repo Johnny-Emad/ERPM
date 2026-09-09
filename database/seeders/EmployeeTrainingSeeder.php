ca02022922376bb3f8e7b2dd4ff216d69de5e220
Johnny-Emad <johnnyjohnnyemnz@gmail.com>
2026-09-09 17:44:22 +0300

    Add EmployeeTrainingSeeder and ProjectSkillSeeder classes; remove old seeder files

--- database/seeders/EmployeeTrainingSeeder.php
+++ database/seeders/EmployeeTrainingSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class employee_training extends Seeder
+class EmployeeTrainingSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- database/seeders/ProjectSkillSeeder.php
+++ database/seeders/ProjectSkillSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class Project_skill extends Seeder
+class ProjectSkillSeeder extends Seeder
 {
     /**
      * Run the database seeds.
