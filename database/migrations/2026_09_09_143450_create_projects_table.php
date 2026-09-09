fa7147d8e57e0170058f14308cadd9704054c0ee
Johnny-Emad <johnnyjohnnyemnz@gmail.com>
2026-09-09 18:24:38 +0300

    Add migration for projects table with title, description, start_date, end_date, and status fields

--- database/migrations/2026_09_09_143450_create_projects_table.php
+++ database/migrations/2026_09_09_143450_create_projects_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->text("description");
+            $table->date("start_date");
+            $table->date("end_date");
+            $table->enum("status", ["Pending", "Active", "Completed"]);
             $table->timestamps();
         });
     }
