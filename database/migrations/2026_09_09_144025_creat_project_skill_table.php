19771f3b92eab95e983997905c4073beb3f46ec2
Johnny-Emad <johnnyjohnnyemnz@gmail.com>
2026-09-10 00:30:33 +0300

    Rename migration table from 'profiles' to 'project_skill'

--- database/migrations/2026_09_09_144025_creat_project_skill_table.php
+++ database/migrations/2026_09_09_144025_creat_project_skill_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('profiles', function (Blueprint $table) {
+        Schema::create('project_skill', function (Blueprint $table) {
             $table->id();
             $table->foreignId("project_id")->constrained()->cascadeOnDelete();
             $table->foreignId("skill_id")->constrained()->cascadeOnDelete();
