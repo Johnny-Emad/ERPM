1efdf1f27402ac80af64623368f6fdf7dd7ebc69
Johnny-Emad <johnnyjohnnyemnz@gmail.com>
2026-09-10 02:03:39 +0300

    Define default state for Department model in DepartmentSeeder

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
+use Deprecated;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,6 @@ class DepartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Department::factory()->count(100)->create();
     }
 }
