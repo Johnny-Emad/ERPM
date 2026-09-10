f753f09d4fbf45de55db353e44498586879846ce
Johnny-Emad <johnnyjohnnyemnz@gmail.com>
2026-09-10 23:12:54 +0300

    feat: implement destroy method in TaskController to handle task deletion

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -77,6 +77,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return redirect()->route('tasks.index')->with('success', 'Task deleted successfully.');
     }
 }
