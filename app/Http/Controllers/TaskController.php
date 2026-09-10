3dfa2fe8b10f6eac8d335aa05ca6e4dcdf914ab8
Johnny-Emad <johnnyjohnnyemnz@gmail.com>
2026-09-10 23:09:59 +0300

    feat: implement create method in TaskController to return create view

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -19,7 +19,10 @@ class TaskController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create() {}
+    public function create()
+    {
+        return view('tasks.create');
+    }
 
     /**
      * Store a newly created resource in storage.
